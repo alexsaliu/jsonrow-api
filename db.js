68af3c10ed6ad1a49cee7da868e3456ea1eb1c3a
Alex <alex.saliu@gmail.com>
2021-05-04 17:43:40 +0930

    Cleaned up db connection and env variables

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const env = process.env.NODE_ENV
 const local_db = process.env.LOCAL_DB
-const live_db = process.env.LIVE_DB
+const live_db = "postgres://kajqcyfe:FFH1iEoAosl6W3NeDiWplCv4hVOtEhNj@rosie.db.elephantsql.com:5432/kajqcyfe"
 
 // POSTGRES DB CONNECTION
 const dbConnection = env === 'development' ? local_db : live_db
